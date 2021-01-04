24d58ef193f04554ed00530cf27742a2407c4ccc
Duy Duong <duyduong@ucla.edu>
2021-01-04 04:40:07 -0800

    added structure to documentation page

--- app/src/components/Help/Help.tsx
+++ app/src/components/Help/Help.tsx
@@ -1,10 +1,56 @@
-import { Typography } from '@material-ui/core'
+import {
+  createStyles,
+  IconButton,
+  makeStyles,
+  Theme,
+  Tooltip,
+  Typography,
+  Zoom,
+} from '@material-ui/core'
+import { Home } from '@material-ui/icons'
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    returnButton: {
+      position: 'fixed',
+      bottom: 0,
+      left: 0,
+      margin: theme.spacing(1),
+    },
+    largeIcon: {
+      fontSize: '2em',
+    },
+    title: {
+      display: 'flex',
+      justifyContent: 'center',
+      marginTop: theme.spacing(1),
+      fontWeight: 400,
+    }
+  })
+)
 
 export default function MainTerminal() {
+  const history = useHistory()
+  const classes = useStyles()
   return (
-    <Typography variant="h5">
-      This is a help page
-    </Typography>
+    <div>
+      <Tooltip
+        title="Return to terminal"
+        TransitionComponent={Zoom}
+        placement="right"
+      >
+        <IconButton
+          className={classes.returnButton}
+          onClick={() => {
+            history.push('/')
+          }}
+        >
+          <Home className={classes.largeIcon} />
+        </IconButton>
+      </Tooltip>
+      <Typography variant="h3" className={classes.title} >Documentation</Typography>
+    </div>
   )
 }
--- app/src/components/MainTerminal/MainTerminal.tsx
+++ app/src/components/MainTerminal/MainTerminal.tsx
@@ -1,12 +1,37 @@
-import { useState } from 'react'
+import {
+  createStyles,
+  IconButton,
+  makeStyles,
+  Theme,
+  Tooltip,
+  Zoom,
+} from '@material-ui/core'
+import { Help } from '@material-ui/icons'
+import React, { useState } from 'react'
 import Terminal from 'react-console-emulator'
+import { useHistory } from 'react-router-dom'
 import { cd } from './functions/cd'
 import { cr } from './functions/cr'
 import { ls } from './functions/ls'
 import { mv } from './functions/mv'
 import { rm } from './functions/rm'
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    helpButton: {
+      position: 'fixed',
+      bottom: 0,
+      right: 0,
+    },
+    largeIcon: {
+      fontSize: '2em',
+    },
+  })
+)
+
 export default function MainTerminal() {
+  const classes = useStyles()
+  const history = useHistory()
   const [workDir, setWorkDir] = useState('/')
 
   const commands = {
@@ -29,31 +54,40 @@ export default function MainTerminal() {
       fn: (...args) => ls(workDir, ...args),
     },
     cr: {
-      description:
-        "Create new files and directories",
+      description: 'Create new files and directories',
       usage: 'cr [-p] PATH [DATA]',
       fn: (...args) => cr(workDir, ...args),
     },
     mv: {
-      description:
-        "Move all source files from source to dest",
+      description: 'Move all source files from source to dest',
       usage: 'mv SOURCE... DEST',
       fn: (...args) => mv(workDir, ...args),
     },
     rm: {
-      description:
-        "Remove a file/folder and all its children",
+      description: 'Remove a file/folder and all its children',
       usage: 'rm PATH',
       fn: (...args) => rm(workDir, ...args),
     },
   }
 
   return (
-    <Terminal
-      commands={commands}
-      welcomeMessage={'Type help to get started'}
-      promptLabel={`me@React:${workDir}$`}
-      style={{minHeight: '100vh'}}
-    />
+    <div>
+      <Terminal
+        commands={commands}
+        welcomeMessage={'Type help or click the question mark to get started'}
+        promptLabel={`me@React:${workDir}$`}
+        style={{ minHeight: '100vh' }}
+      />
+      <Tooltip title="Help page" TransitionComponent={Zoom} placement="left">
+        <IconButton
+          className={classes.helpButton}
+          onClick={() => {
+            history.push('/help')
+          }}
+        >
+          <Help className={classes.largeIcon} />
+        </IconButton>
+      </Tooltip>
+    </div>
   )
 }
